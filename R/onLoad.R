9bba26fca3a71affe6c042b748f1484ce24d4791
Michael Hahsler <mhahsler@smu.edu>
2023-12-14 13:16:30 -0600

    We use now rJava.

---
+++ R/onLoad.R
@@ -0,0 +1,7 @@
+.onLoad <-
+  function(libname, pkgname)
+  {
+    .jpackage(pkgname, 
+              morePaths = system.file("java/rdp_classifier.jar", package="rRDP"), 
+              lib.loc = libname)
+  }
